e99df8a0fb5dd1953029174e7c4c260d1cc871a1
Braeden Christensen <braeden.j.christensen@gmail.com>
2026-07-27 10:32:34 -0600

    Updated header comment in proc.py
    Expanded scope to prevent future painpoints by permitting
    Linux application calls from proc.py.

--- utils/proc.py
+++ utils/proc.py
@@ -2,11 +2,16 @@
 # used by other functions elsewhere.
 # The Proc class is scoped to ONLY READ details from the /proc filesystem.
 # Anything other work must be done outside this class.
+# For sake of sanity, we are going to also use programs inside of linux
+# that reads information from /proc, because some of /proc is not human
+# readable.
+# Maybe later, we can learn how to parse this information
 
 class Proc:
     # Constants
     # Proc Folder Locations
     PROC_ROOT = "/proc/"
+
     # Proc files
     CPUINFO = "cpuinfo"
     MDSTAT = "mdstat"
