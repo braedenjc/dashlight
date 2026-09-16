54f2268722f2c6eca5933663ce4e6106486953a6
Braeden Christensen <braeden.j.christensen@gmail.com>
2026-09-16 15:20:21 -0600

    hwmon.py: Organized, added finding logic
    Organized hwmon.py, added logic to populate the dictionary
    with reading names and Sensor objects.

--- classes/hwmon.py
+++ classes/hwmon.py
@@ -3,14 +3,17 @@
 # This hwmon object is usually a folder inside of /sys/class,
 # so we make an object that represents that folder.
 # Inside of each hwmon folder there are one
-# or more sensors. We need to track
-# those sensors and give easy access to them.
+# or more file that tells us:
+# - A name for the hwmon
+# - sensor readings
+# We need to track those sensors and give easy access to them.
 
 '''
 TODO: Add a method to detect all of the sensors, and designate a type of sensor
         Implement the Factory programming pattern for use in find_sensors().
 '''
 from pathlib import Path
+from sensors import Sensor
 
 
 class Hwmon:
@@ -22,18 +25,19 @@ class Hwmon:
 
     def __init__(self, name, fs_location):
         #  When we init a hwmon object, we are representing a folder that
-        # that contains files, which symbolize sensors.
+        # that contains files, which symbolize sensor readings.
         # The process that init should follow is:
-        # 1) Get the hwmon folder's name.
-        # 2) Get the hwmon folder's location
-        # 3) Collect all of the sensors inside of
-        #     hwmon inside of a dictionary.
-        #     We need to detect the type of sensor we are making
-        #     and add it into the sensors dict appropriately.
-        #     The mapping of the sensors dict should be: (sensor name, sensor)
-        self.__hwmon_name__ = name
+        # 1) Store the hwmon folder's location.
+        # 2) Get the hwmon --DEVICE'S-- name from the hwmon folder's name file.
+        # 3) Assign the name to this hwmon's object.
+        # 4) Collect all of the sensor files inside of
+        # the device directory, and assign names in a dict.
+
         self.__hwmon_location__ = fs_location
+        self.__hwmon_name__ = self.__get_hwmon_name__()
         self.__sensors_dict__ = self.__find_sensors__()
+
+    # PRIVATE METHODS:
 
     # A helper method to help locate the sensors inside of hwmon.
     def __find_sensors__(self):
@@ -44,19 +48,30 @@ class Hwmon:
         fileList = [
             sensor for sensor in hwmon_path.iterdir() if sensor.is_file()
             ]
+        sensors_dict = dict()
         for sensorFile in fileList:
-            fileName = sensorFile.name
-            if fileName == "name":
-                print("Name was found! It is:")
-                with open(self.__hwmon_location__ + "/" + fileName) as sensorName:
-                    print(sensorName.readline())
-        return None
+            if sensorFile.name != "name":
+                sensors_dict[sensorFile.name] = Sensor(
+                    sensorFile.name, sensorFile.absolute()
+                )
+        return sensors_dict
+
+    def __get_hwmon_name__(self):
+        tempName = ''
+        with open(self.__hwmon_location__ + "/" + "name") as hwmonName:
+            tempName = hwmonName.readline().strip()
+        return tempName
+
+    # PUBLIC METHODS
+
+    def get_device_name(self):
+        return self.__hwmon_name__
 
     def get_sensors_of_type(self):
         return None
 
     def get_all_sensors(self):
-        return None
+        return self.__sensors_dict__
 
     def get_sensor_reading(self, name):
-        return self.__sensors_dict__[name].get_sensor_reading()
+        return self.__sensors_dict__[name].get_raw_sensor_reading()
